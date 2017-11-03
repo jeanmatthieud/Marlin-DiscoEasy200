2485e26853ff889768ae465e7da22f5013155674
Jean-Matthieu DECHRISTE <jeanmatthieu@dechriste.fr>
2017-11-03 13:43:50 +0100

    Fix endstops positions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -798,12 +798,12 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -7
-#define Y_MIN_POS -7
+#define X_MIN_POS -2
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Y_MAX_POS Y_BED_SIZE + 5
+#define Z_MAX_POS 195
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
 #define MIN_SOFTWARE_ENDSTOPS
